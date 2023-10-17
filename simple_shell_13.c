607284e3df7c93021aa5b1d6aa4b39cc3cd9a02a
ronaldnairobi <135117343+ronaldnairobi@users.noreply.github.com>
2023-10-17 11:46:40 +0300

    Create simple_shell_13.c
    simple shell file 13

---
+++ simple_shell_13.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+
+extern char **environ;  // Declare the external environment variable
+
+// Function to set environment variables
+int custom_setenv(const char *variable, const char *value) {
+    if (variable == NULL || value == NULL) {
+        fprintf(stderr, "setenv: Missing arguments\n");
+        return -1;
+    }
+
+    if (setenv(variable, value, 1) != 0) {
+        fprintf(stderr, "setenv: Failed to set environment variable\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+// Function to unset environment variables
+int custom_unsetenv(const char *variable) {
+    if (variable == NULL) {
+        fprintf(stderr, "unsetenv: Missing argument\n");
+        return -1;
+    }
+
+    if (unsetenv(variable) != 0) {
+        fprintf(stderr, "unsetenv: Failed to unset environment variable\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+// Function to execute a single command
+int execute_command(const char *command) {
+    if (command == NULL) {
+        return 0;
+    }
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process
+        if (execlp(command, command, NULL) == -1) {
+            perror("execlp");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+        return WEXITSTATUS(status);
+    }
+
+    return 0;
+}
+
+// Function to handle alias operations
+void handle_alias(const char *arguments) {
+    char *aliases[100];  // You can adjust the array size as needed
+    int alias_count = 0;
+
+    // Tokenize the arguments into aliases
+    char *token = strtok(arguments, " ");
+    while (token != NULL) {
+        // Check if the token contains '='
+        char *equal_sign = strchr(token, '=');
+
+        if (equal_sign != NULL) {
+            // Split the token into name and value
+            char *name = token;
+            *equal_sign = '\0';
+            char *value = equal_sign + 1;
+            aliases[alias_count] = name;
+
+            // Define or update an alias
+            custom_setenv(name, value);
+        } else {
+            // Print the alias value if it exists
+            char *value = getenv(token);
+            if (value != NULL) {
+                printf("%s='%s'\n", token, value);
+            }
+
+            aliases[alias_count] = token;
+        }
+
+        alias_count++;
+
+        token = strtok(NULL, " ");
+    }
+
+    // If no aliases are provided, list all aliases
+    if (alias_count == 0) {
+        for (int i = 0; i < alias_count; i++) {
+            char *value = getenv(aliases[i]);
+            if (value != NULL) {
+                printf("%s='%s'\n", aliases[i], value);
+            }
+        }
+    }
+}
+
+int main(void) {
+    char *input = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    while (1) {
+        printf("simple_shell$ ");  // Display the prompt
+        nread = getline(&input, &len, stdin);
+
+        if (nread == -1) {
+            if (feof(stdin)) {
+                printf("\n");  // Handle "Ctrl+D" - end of file condition
+                break;
+            } else {
+                perror("getline");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // Remove the newline character
+        input[strcspn(input, "\n")] = '\0';
+
+        int prev_status = 0;  // Store the status of the previous command
+
+        // Tokenize the input into individual commands based on ';'
+        char *token = strtok(input, ";");
+        while (token != NULL) {
+            // Trim leading and trailing whitespaces from the command
+            char *command = token;
+            while (*command == ' ' || *command == '\t') {
+                command++;
+            }
+            char *end = command + strlen(command) - 1;
+            while (end > command && (*end == ' ' || *end == '\t')) {
+                *end-- = '\0';
+            }
+
+            if (strlen(command) > 0) {
+                // Execute the individual command
+                int status = execute_command(command);
+
+                // Tokenize the input further for '&&' and '||' operators
+                char *next_operator = strtok(NULL, " ");
+                if (next_operator != NULL) {
+                    if (strcmp(next_operator, "&&") == 0) {
+                        if (status == 0) {
+                            prev_status = 0;
+                        } else {
+                            break;  // Skip the next command
+                        }
+                    } else if (strcmp(next_operator, "||") == 0) {
+                        if (status != 0) {
+                            prev_status = status;
+                        } else {
+                            break;  // Skip the next command
+                        }
+                    }
+                }
+            }
+
+            token = strtok(NULL, ";");
+        }
+    }
+
+    free(input);
+    return 0;
+}
