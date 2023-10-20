5fbf3c7028bb1064592b940987905d80ea0f72a0
ronaldnairobi <ronaldjnr3@gmail.com>
2023-10-20 09:17:18 -0700

    modified

--- simple_shell_1.c
+++ simple_shell_1.c
@@ -5,46 +5,47 @@
 
 int main(void)
 {
-    char *input = NULL;
-    size_t len = 0;
-    ssize_t nread;
-
-    while (1) {
-        printf("simple_shell$ ");  // Display the prompt
-        nread = getline(&input, &len, stdin);
-
-        if (nread == -1) {
-            if (feof(stdin)) {
-                printf("\n");  // Handle "Ctrl+D" - end of file condition
-                break;
-            } else {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        // Remove the newline character
-        input[strcspn(input, "\n")] = '\0';
-
-        // Create a child process to execute the command
-        pid_t pid = fork();
-
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            if (execve(input, NULL, NULL) == -1) {
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // Parent process
-            int status;
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    free(input);
-    return 0;
+char *input = NULL;
+size_t len = 0;
+ssize_t nread;
+while (1)
+{
+printf("simple_shell$ ");
+nread = getline(&input, &len, stdin);
+if (nread == -1)
+{
+if (feof(stdin))
+{
+printf("\n");
+break;
+}
+else
+{
+perror("getline");
+exit(EXIT_FAILURE);
+}
+}
+input[strcspn(input, "\n")] = '\0';
+pid_t pid = fork();
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+if (execve(input, NULL, NULL) == -1)
+{
+perror("execve");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+int status;
+waitpid(pid, &status, 0);
+}
+}
+free(input);
+return 0;
 }
